212b1b681131d3f1822782713b8a3dcace35f001
Bernardo Baumblatt <baumblatt@gmail.com>
2020-08-29 21:12:52 -0300

    New Bottom Sheet component type and constructors for Container and Dialog component types.

--- src/component/component.ts
+++ src/component/component.ts
@@ -13,20 +13,17 @@ import {
 import {createDefaultPath, getWorkspace} from "@schematics/angular/utility/workspace";
 import {addMixin, getAngularSchematicsDefaults, makeChanges} from "../utils/utils";
 import {Path, strings} from "@angular-devkit/core";
-import {findModuleFromOptions} from "@schematics/angular/utility/find-module";
+import {buildRelativePath, findModuleFromOptions} from "@schematics/angular/utility/find-module";
 import {parseName} from "@schematics/angular/utility/parse-name";
+import {insertImport} from "@schematics/angular/utility/ast-utils";
+import {classify} from "@angular-devkit/core/src/utils/strings";
+import ts = require("typescript");
+import {ReplaceChange} from "@schematics/angular/utility/change";
 
 export function factory(_options: Component): Rule {
 
 
   return async (_tree: Tree, _context: SchematicContext) => {
-    if (!_options.path) {
-      _options = {..._options,
-        path: 'projects/my-test-drive/src/app/core',
-        project: 'my-test-drive',
-      };
-    }
-
     const workspace = await getWorkspace(_tree);
     const project = workspace.projects.get(_options.project as string);
 
@@ -35,13 +32,16 @@ export function factory(_options: Component): Rule {
     }
 
     const modulePath = findModuleFromOptions(_tree, _options) as Path;
-    // const moduleFile = modulePath.split('/').pop() || '';
-    // const moduleName = moduleFile.split('.').reverse().pop() || '';
+    const moduleFile = modulePath.split('/').pop() || '';
+    const moduleDir = modulePath.substring(0, modulePath.indexOf(moduleFile));
+    const moduleName = moduleFile.split('.').reverse().pop() || '';
 
     const parsedPath = parseName(_options.path as string, _options.name);
     _options.name = parsedPath.name;
     _options.path = parsedPath.path;
     _options.selector = _options.selector || buildSelector(_options, project && project.prefix || '');
+
+    console.log('path after after', _options.path);
 
     const name = strings.dasherize(_options.name);
 
@@ -70,16 +70,138 @@ export function factory(_options: Component): Rule {
         const changes = addMixin(
           _tree,
           parentStylePath,
-          `/${_options.path}/${name}/${name}.${_options.type}.theme`,
+          `${_options.path}/${name}/${name}.${_options.type}.theme`,
           `${_options.selector}-${_options.type}-theme`
         );
 
         return makeChanges(_tree, parentStylePath, changes);
-      }
+      },
       // the container specific steps
+      (_tree) => {
+        if (_options.type === 'container') {
+          const componentPath = `${_options.path}/${name}/${name}.${_options.type}.ts`;
+
+          const source = ts.createSourceFile(
+            componentPath,
+            _tree.read(componentPath.substring(1))?.toString() || '',
+            ts.ScriptTarget.Latest, true
+          );
+
+          const relativePath = buildRelativePath(componentPath,
+            `${moduleDir}/store/reducers/feature.reducer`);
+
+          const changes = [insertImport(
+            source,
+            componentPath,
+            `${classify(moduleName)}State`,
+            relativePath
+          )];
+
+          changes.push(insertImport(
+            source,
+            componentPath,
+            `Store`,
+            '@ngrx/store'
+          ));
+
+          changes.push(replaceConstructor(
+            source,
+            componentPath,
+            `(private store: Store<${classify(moduleName)}State>)`
+          ));
+
+          return makeChanges(_tree, componentPath, changes);
+        }
+      },
       // the dialog specific steps
+      (_tree) => {
+        if (_options.type === 'dialog') {
+          const componentPath = `${_options.path}/${name}/${name}.${_options.type}.ts`;
+
+          const source = ts.createSourceFile(
+            componentPath,
+            _tree.read(componentPath.substring(1))?.toString() || '',
+            ts.ScriptTarget.Latest, true
+          );
+
+          const changes = [insertImport(
+            source,
+            componentPath,
+            'MatDialogRef, MAT_DIALOG_DATA',
+            '@angular/material/dialog'
+          )];
+
+          changes.push(insertImport(
+            source,
+            componentPath,
+            'Inject',
+            '@angular/core'
+          ));
+
+          changes.push(replaceConstructor(
+            source,
+            componentPath,
+            `(public dialogRef: MatDialogRef<${classify(name)}Dialog>, @Inject(MAT_DIALOG_DATA) public data: any)`
+          ));
+
+          return makeChanges(_tree, componentPath, changes);
+        }
+      },
+      // the bottom-sheet specific steps
+      (_tree) => {
+        if (_options.type === 'bottom-sheet') {
+          const componentPath = `${_options.path}/${name}/${name}.${_options.type}.ts`;
+
+          const source = ts.createSourceFile(
+            componentPath,
+            _tree.read(componentPath.substring(1))?.toString() || '',
+            ts.ScriptTarget.Latest, true
+          );
+
+          const changes = [insertImport(
+            source,
+            componentPath,
+            'MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA',
+            '@angular/material/bottom-sheet'
+          )];
+
+          changes.push(insertImport(
+            source,
+            componentPath,
+            'Inject',
+            '@angular/core'
+          ));
+
+          changes.push(replaceConstructor(
+            source,
+            componentPath,
+            `(private bottomSheetRef: MatBottomSheetRef<${classify(name)}BottomSheet>, @Inject(MAT_BOTTOM_SHEET_DATA) public data: any)`
+          ));
+
+          return makeChanges(_tree, componentPath, changes);
+        }
+      },
     ]);
   }
+}
+
+function replaceConstructor(source: ts.SourceFile, componentPath: string, newConstructor: string) {
+  const componentClass = source.statements.find(
+    (stm: any) => stm.kind === ts.SyntaxKind.ClassDeclaration
+  ) as ts.ClassDeclaration;
+
+  const componentConstructor = componentClass.members.find(
+    (member) => member.kind === ts.SyntaxKind.Constructor
+  ) as ts.ConstructorDeclaration;
+
+  const [start, end] = componentConstructor.getText().split('()');
+
+  return new ReplaceChange(
+    componentPath,
+    componentConstructor.pos,
+    `  ${componentConstructor.getText()}\n\n`,
+    `\n\n  ${[start, newConstructor, end].join('')}`
+  );
 }
 
 function buildSelector(options: Component, projectPrefix: string) {
